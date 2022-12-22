b7985d47c58efbc5a149d81127791884a1245b68
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-12-21 18:45:58 -0600

    some icons - looking good

--- plow_the_sidewalks_criteria_app/ui.R
+++ plow_the_sidewalks_criteria_app/ui.R
@@ -22,10 +22,16 @@ ui <- fluidPage(
                    alt = "Access Living logo",
                    class = "pull-right"
                  ),
+                 img(
+                   src = "second-logo.png",
+                   height = "100px",
+                   alt = "Better streets logo",
+                   class = "pull-right"
+                 ),
                  HTML(
                    "<h1><section style='font-family: Poppins, sans-serif;
       font-size:4rem; font-weight: bold; color: #270075'>
-    #PlowTheSidewalks pilot zone explorer</h3>"
+    #Plow The Sidewalks pilot zone explorer</h3>"
                  )
                ),
     windowTitle = "Where should we #PlowTheSidewalks first?"),
@@ -56,15 +62,25 @@ ui <- fluidPage(
     Where should Chicago try municipal sidewalk plowing first?</h3>"
       ),
     p(
-      "We are drafting an ordinance asking Chicago
+      "Better Streets Chicago and Access Living are drafting an ordinance asking Chicago
           to set aside $750,000 in the upcoming budget
-          for a municipal sidewalk clearing pilot program –
-          a small test of what a city-wide program will look like."
+          for a municipal sidewalk clearing pilot program.
+          This pilot will serve as a small test of what a city-wide program
+          could look like."
     ),
+    
     p(
-      "This interactive page explores where that pilot program should occur --
-      and how to prioritize communities in the process."
-    )),
+      "The draft ordinance calls for the city to run the pilot in four,
+      2.5 square-mile zones across the city, with an eye towards mobility justice."
+    ),
+    
+    p(
+      'This interactive page asks, "Where should we try sidewalk plowing first?",
+      and translates our priorities into a map of places
+      where a test of sidewalk snow plowing would have greatest impact 
+      for the people that need it most.'
+    )
+    ),
     ## 1: Priorities -----
     fluidRow(
       HTML(
@@ -85,19 +101,86 @@ ui <- fluidPage(
       HTML(
         "<h3><section style='font-family: Poppins, sans-serif;
       font-size:2rem; font-weight: bold; color: #270075'>
-    Finding places that match all of these priorities is probably
-    impossible</h3>"
+    No one neighborhood will best serve
+    <span style='color: #9b51e0'>
+    all
+    </span>of these priorities at once</h3>"
       ),
-    p(
-      "To maximize the pilot program’s impact,
-          we are asking the city to locate the pilot zones
-          in a way that prioritizes:"
+    HTML(
+      "<p>That is why we are suggesting the city take a
+      measured approach to the problem
+      of pilot zone placement, and create
+      <span style='color: #9b51e0; font-weight:bold'>
+      two types</span>
+      of pilot zones:</p>"
+    )
     ),
     
-    wellPanel(p("Some maps by priority"))
+    wellPanel(fluidRow(
+      column(
+        6,
+        HTML(
+          "<p style = 'text-align: center'>One set of zones will focus on
+      <span style='color: #270075; font-weight:bold'>
+      people with disabilities and elders</span></p>"
+        ),
+      fluidRow(
+        align = 'center',
+        fontawesome::fa(
+          "wheelchair-move",
+          fill = "#270075",
+          height = '30px'
+        ),
+        fontawesome::fa("user-plus",
+                        fill = "#270075",
+                        height = '30px')
+      )
+      ),
+      
+      column(
+        6,
+        HTML(
+          "<p style = 'text-align: center'>A second set of zones will focus on
+      <span style='color: #270075; font-weight:bold'>
+      transit activity, zero-car households and children</span></p>"
+        ),
+      fluidRow(
+        align = 'center',
+        fontawesome::fa("bus",
+                        fill = "#270075",
+                        height = '30px'),
+        fontawesome::fa("car",
+                        fill = "#270075",
+                        height = '30px'),
+        fontawesome::fa("baby-carriage",
+                        fill = "#270075",
+                        height = '30px'),
+        
+      )
+      )
     ),
+    br(),
+    fluidRow(column(
+      12,
+      HTML(
+        "<p style = 'text-align: center'>Both sets of zones will still give some weight to
+      <span style='color: #270075; font-weight:bold'>
+      population density and known problem areas.</span></p>"
+      ),
+      fluidRow(
+        align = 'center',
+        fontawesome::fa(
+          "city",
+          fill = "#270075",
+          height = '40px'
+        ),
+        fontawesome::fa("building-circle-exclamation",
+                        fill = "#270075",
+                        height = '40px')
+      )
+    ))),
     
-    # 2: Priorities, mapped -----
+    # Map our priorities -----
     fluidRow(scrolly_container(
       "scr",
       scrolly_graph(textOutput("section"),
@@ -143,6 +226,6 @@ ui <- fluidPage(
              text-align: right;
              padding: 1%;",
     align = "right",
-    class = "pull-down"
+    class = "pull-down-right"
   )
 )
