cc50c10cfa6d303fda0a13017da2a99349f3ada5
Ashley Asmus <ashley.asmus@metc.state.mn.us>
2022-12-23 19:15:38 -0600

    add score reactivity

--- plow_the_sidewalks_criteria_app/server.R
+++ plow_the_sidewalks_criteria_app/server.R
@@ -1,17 +1,256 @@
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-  
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    col <- input$scr
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = col, border = 'white')
+server <- function(input, output, session) {
+  #  render scrollytell --------
+  output$scr <- renderScrollytell({
+    scrollytell()
   })
   
-  output$scr <- renderScrollytell({scrollytell()})
-  output$section <- renderText(paste0("Section: ", input$scr))
+  output$scr2 <- renderScrollytell({
+    scrollytell()
+  })
+
+  # update weights ------
+  weights <- reactiveVal(first_weights)
+
+  observe({
+    input_weights <- list(
+      "dis_w" = input$s_dis,
+      "old_w" = input$s_old,
+      "kid_w" = input$s_kid,
+      "den_w" = input$s_den,
+      "zca_w" = input$s_zca,
+      "cta_w" = input$s_cta,
+      "bad_w" = input$s_bad
+    )
+
+    weights(input_weights)
+  })
   
-  observe({cat("section:", input$scr, "\n")})
+  # change weights for scrolling ----
+  observeEvent(input$scr, ignoreNULL = T, ignoreInit = T, 
+               {
+    
+    if(input$scr == "equal"){
+      weights(first_weights)
+    }
+    
+    if(input$scr == "disabilities"){
+      new_weights <- list(
+        "dis_w" = 0.4,
+        "old_w" = 0.4,
+        "kid_w" = 0,
+        "den_w" = 0.1,
+        "zca_w" = 0,
+        "cta_w" = 0,
+        "bad_w" = 0.1
+      )
+      
+      weights(new_weights)
+        
+    }
+                 
+     if(input$scr == "transit"){
+       new_weights <- list(
+         "dis_w" = 0,
+         "old_w" = 0,
+         "kid_w" = 0.25,
+         "den_w" = 0.15,
+         "zca_w" = 0.25,
+         "cta_w" = 0.25,
+         "bad_w" = 0.15
+       )
+       
+       weights(new_weights)
+       
+     }
+  })
+
+  # update tract scores -----
+  scores <- reactiveVal()
+
+  observe({
+    updated_scores <-
+      master %>%
+      mutate(across(
+        c(matches(
+          "pct_pop|pct_hh"
+        )),
+        # get a scaled value for each variable
+        ~ scale(
+          .,
+          center = min(.), scale = diff(range(.))
+        )[, 1],
+        .names = "{sub('pct_pop|pct_hh', 'scale', col)}"
+      )) %>%
+      # scale variables with non-standard names:
+      mutate(
+        den_scale =
+          scale(den, center = min(den), scale = diff(range(den)))[, 1],
+        bad_scale = scale(
+          n_bad_permi2,
+          center = min(n_bad_permi2),
+          scale = diff(range(n_bad_permi2))
+        )[, 1],
+        cta_scale = scale(
+          cta_activity,
+          center = min(cta_activity),
+          scale = diff(range(cta_activity))
+        )[, 1]
+      ) %>%
+      select(GEOID, contains("scale")) %>%
+      # calculate a weighted score
+      mutate(
+        score =
+        # Demographics
+          (amb_scale * weights()$dis_w * 0.5) +
+            (vis_scale * weights()$dis_w * 0.5) +
+            (old_scale * weights()$old_w) +
+            (kid_scale * weights()$kid_w) +
+            (zca_scale * weights()$zca_w) +
+            # Land use and transportation
+            (den_scale * weights()$den_w) +
+            (cta_scale * weights()$cta_w) +
+            (bad_scale * weights()$bad_w)
+      ) %>%
+      mutate(score_pctile = ntile(desc(score), 100))
+
+    # Feed reactive value
+    scores(updated_scores)
+  })
+
+  # base map -----
+  output$mapBuild <- renderLeaflet({
+    leaflet(options = leafletOptions(minZoom = 10, maxZoom = 10,
+                                     zoomControl = F,
+                                     attributionControl = FALSE)) %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      fitBounds(
+        lat1 = chi_bbox[["ymin"]],
+        lat2 = chi_bbox[["ymax"]],
+        lng1 = chi_bbox[["xmin"]],
+        lng2 = chi_bbox[["xmax"]]
+      ) %>%
+      setView(lat = 
+                41.840675, 
+              lng = 
+                -87.679365,
+              zoom = 10)
+  })
+
+  
+  # update map with new scores -----
+  observe({
+    map_data <- scores() %>%
+      mutate(helptext = ifelse(
+        score_pctile > 50,
+        paste0(
+          "bottom ",
+          100 - round(score_pctile)
+        ),
+        paste0(
+          "top ",
+          round(score_pctile)
+        )
+      ))
+
+    color_data <- map_data$score_pctile
+    my_title <- "Rank"
+    my_pal <-
+      colorBin("plasma",
+        color_data,
+        10,
+        pretty = T,
+        reverse = T
+      )
+
+
+    tooltips <- sprintf(
+      "<p style='font-family: Poppins, sans-serif;
+      font-size:1rem;
+      color: #270075'>
+      This tract ranks in the
+      <br>
+      <strong><span style='font-size:1.5rem'>
+      %s%% of tracts</strong>
+      <br>
+      </span>,
+      for the priorities you selected.",
+      map_data$helptext
+    ) %>%
+      lapply(htmltools::HTML)
+
+    leafletProxy("mapBuild", data = map_data) %>%
+      clearShapes() %>%
+      addPolygons(
+        data = map_data,
+        layerId = ~GEOID,
+        label = ~tooltips,
+        stroke = FALSE,
+        fillOpacity = 0.4,
+        fillColor = my_pal(color_data)
+      )
+  })
+
+  # force sliders add to 100------
+  observeEvent(input$s_dis, {
+    update_slider_weights(input, "s_dis")
+  })
+
+
+  observeEvent(input$s_old, {
+    update_slider_weights(input, "s_old")
+  })
+
+  observeEvent(input$s_kid, {
+    update_slider_weights(input, "s_kid")
+  })
+
+  observeEvent(input$s_den, {
+    update_slider_weights(input, "s_den")
+      })
+
+  observeEvent(input$s_zca, {
+    update_slider_weights(input, "s_zca")
+  })
+
+  observeEvent(input$s_cta, {
+    update_slider_weights(input, "s_cta")
+  })
+
+  observeEvent(input$s_bad, {
+    update_slider_weights(input, "s_bad")
+  })
+  
+  
+  
+  # map for drawing --------------
+  output$mapDraw <- renderLeaflet({
+    leaflet(options = leafletOptions(minZoom = 10, maxZoom = 10,
+                                     zoomControl = F,
+                                     attributionControl = FALSE)) %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      fitBounds(
+        lat1 = chi_bbox[["ymin"]],
+        lat2 = chi_bbox[["ymax"]],
+        lng1 = chi_bbox[["xmin"]],
+        lng2 = chi_bbox[["xmax"]]
+      ) %>%
+      setView(lat = 
+                41.840675, 
+              lng = 
+                -87.679365,
+              zoom = 10) %>%
+      # draw rectangle tool ----
+    leaflet.extras::addDrawToolbar(
+      position = "topright",
+      polylineOptions = FALSE,
+      circleOptions = FALSE,
+      polygonOptions = FALSE,
+      # rectangleOptions = FALSE,
+      markerOptions = FALSE,
+      circleMarkerOptions = FALSE,
+      singleFeature = TRUE
+    )
+      
+  })
 }
